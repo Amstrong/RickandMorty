09600c4e11830b5279f6667001d916388deee972
Amstrong <monachelloa@uninorte.edu.co>
2020-09-25 17:20:51 -0500

    feat:Adding related to redux (Actions, reducers, store).

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.styl";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reducers from "../src/reducers/index";
+import reduxThunk from "redux-thunk";
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancer(applyMiddleware(reduxThunk))
+);
 
 var mountNode = document.getElementById("app");
 document.body.innerHTML = '<div id="app"></div>';
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import characterReducer from "./charactersReducer"
+export default combineReducers({
+    characterReducer
+})
