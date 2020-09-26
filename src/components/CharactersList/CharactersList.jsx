e7b73472ce6f0a240e777bb01367ade6a08d7fa6
Amstrong <monachelloa@uninorte.edu.co>
2020-09-25 22:29:18 -0500

    feat: Adding CharactersList in Components

--- src/components/CharactersList/CharactersList.jsx
+++ src/components/CharactersList/CharactersList.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import "../CharactersList/CharactersList.styl";
+import "./CharactersList.styl";
 function CharactersList({ list }) {
   return (
     <div>
       {!list.length ? (
         console.log("No hay")
       ) : (
-        <div>
+        <div className="container__characterList">
           {list.map((character) => {
-            return <p key={character.id}>{character.name}</p>;
+            return (
+              <div key={character.id} className="container--character">
+                <div className="photo">
+                  <img src={character.image} alt={character.name} className="photo__character"/>
+                </div>
+                <div className="character__properties">
+                <div> 
+                  <p>{character.name}</p>
+                  <p>{character.species} - {character.status}</p>
+                  <p>{character.gender}</p>
+                  <p>{character.location.name}</p>
+                  </div>
+                </div>
+              </div>
+            );
           })}
         </div>
       )}
